b201add4ba27173073df4beacdebd5fb0305eeed
coca-in-a-cola <67217968+coca-in-a-cola@users.noreply.github.com>
2022-12-06 12:23:25 +0500

    quick fix

--- api/middleware/du_task_number.py
+++ api/middleware/du_task_number.py
@@ -1,11 +1,13 @@
 from datetime import date
+from definitions import basedir
+import os
 import json
 
 sequenceNumber = 0
 lastDate = None
 
 try:
-    read_number = open('du_sequence.json', 'r')
+    read_number = open(os.path.join(basedir, du_sequence.json), 'r')
     sequenceNumber, lastDate = json.load(read_number)
     read_number.close()
 except:
@@ -22,7 +24,7 @@ def du_task_number():
 
     sequenceNumber += 1
 
-    save_number = open('du_sequence.json', 'w')
+    save_number = open(os.path.join(basedir, du_sequence.json), 'w')
     save_number.write(json.dumps([sequenceNumber, lastDate]))
     save_number.close()
 
